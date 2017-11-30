e194b358f021d720ee4ba962478eddd9d47c4d49
Clarissa Knox <knox.ck@gmail.com>
2017-11-30 17:26:23 +0000

    routing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :smoothies, :smoothie]
 
   def home
   end
+
+  def smoothies
+  end
+
+  def smoothie
+  end
+
 end
