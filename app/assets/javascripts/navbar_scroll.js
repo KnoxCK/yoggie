cdf15a31aaf9d19e0ddfa4ae31e1c562bf4d02e1
Clarissa Knox <knox.ck@gmail.com>
2018-02-08 12:34:16 +0000

    landing & navbar

---
+++ app/assets/javascripts/navbar_scroll.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll > $(window).height() * 0.99) {
+        $(".navbar-yoggie").addClass("hidden");
+        $(".navbar-yoggie-scroll").removeClass("hidden");
+    } else if (scroll < $(window).height()) {
+        $(".navbar-yoggie").removeClass("hidden");
+        $(".navbar-yoggie-scroll").addClass("hidden");
+    }
+});
