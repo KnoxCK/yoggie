a8f1205c050961e07b9508899c2c6436338dae7e
Guv0 <govinder.marwaha@gmail.com>
2018-07-30 19:51:08 +0100

    stripe config

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
-  :secret_key      => ENV['STRIPE_SECRET_KEY']
+  :publishable_key => 'pk_test_hveOGYFAnnyFAoTY5OV01p26',
+  :secret_key      => 'sk_test_8CoEsV9ijSTj6OBoyQPjJl1C'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
