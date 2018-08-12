ce219eb7feb1a90d775a351464fec107fff13548
Guv0 <govinder.marwaha@gmail.com>
2018-08-12 12:42:04 +0100

    valid postcode outcodes

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
 
-  DELIVERY_AREAS = ['SW17']
+  DELIVERY_AREAS = ['W9', 'W8', 'W6', 'W2', 'W1H', 'W14', 'W12', 'W11', 'SW8', 'SW9', 'SW8', 'SW7', 'SW3', 'SW4', 'SW5',
+                    'SW6', 'SW2', 'SW17', 'SW18', 'SW19', 'SW15', 'SW12', 'SW11', 'SW10', 'NW8', 'NW1', 'SW1W', 'SW1X',
+                    'SW1V', 'SW1P', 'SW1A', 'SW1H', 'SW1E']
 end
