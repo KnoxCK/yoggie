330a189d5f19fa15e0d3a6abd4d681244f30805f
Guv0 <govinder.marwaha@gmail.com>
2018-08-20 18:56:49 +0100

    postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "yoggie_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { ENV["POSTMARK_API_KEY"] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
