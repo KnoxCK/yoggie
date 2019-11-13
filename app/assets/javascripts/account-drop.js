c7b4833491ea801ddc922c73148067fd368fcd2a
Clarissa Knox <knox.ck@gmail.com>
2019-11-13 18:42:23 +0000

    finalising changes

---
+++ app/assets/javascripts/account-drop.js
@@ -0,0 +1,16 @@
+$(document).on('click', '#details-click', function(e) {
+    $('.details').toggle("slide");
+  e.preventDefault();
+});
+$(document).on('click', '#next-box-click', function(e) {
+    $('.next-box').toggle("slide");
+  e.preventDefault();
+});
+$(document).on('click', '#delivery-click', function(e) {
+    $('.delivery').toggle("slide");
+  e.preventDefault();
+});
+$(document).on('click', '#order2-click', function(e) {
+    $('.order2').toggle("slide");
+  e.preventDefault();
+});
