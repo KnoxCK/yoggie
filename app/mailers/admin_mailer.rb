eada329f0978893b8d2a96cbf051f8af5d644901
Jake Johnson <johnson.s.jake@gmail.com>
2019-12-19 12:38:17 +0100

    attach csv to several admin mailers

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -18,6 +18,9 @@ class AdminMailer < ApplicationMailer
     #charlie@yoggie.com
     @basket = basket
     @customer = basket.customer
+    csv = @customer.generate_order_csv
+
+    attachments["smoothie_change_#{@customer&.basket&.stripe_sub_id}.csv"] = {mime_type: 'text/csv', content: csv}
     mail(
       to: 'info@yoggie.co.uk',
       subject: 'Smoothie Change')
@@ -42,6 +45,9 @@ class AdminMailer < ApplicationMailer
   def subscription_change(customer)
     #charlie@yoggie.com
     @customer = customer
+    csv = @customer.generate_order_csv
+
+    attachments["subscription_change_#{@customer&.basket&.stripe_sub_id}.csv"] = {mime_type: 'text/csv', content: csv}
     mail(
       to: 'info@yoggie.co.uk',
       subject: 'Changed Subscription')
